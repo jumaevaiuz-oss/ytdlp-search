9c83d47718917b62dec426b29715c332179d5e41
jumaevaiuz-oss <jumaev.aiuz@gmail.com>
2026-08-16 21:48:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import urllib.request
 app = Flask(__name__)
 
 # API himoya kaliti - buni o'zgartiring!
-API_KEY = os.environ.get("API_KEY", "mening_maxfiy_kalitim")
+API_KEY = os.environ.get("API_KEY", "Kodim77a667_yz")
 
 DOWNLOAD_DIR = "/tmp/downloads"
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
