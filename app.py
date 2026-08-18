0add7c98cbfeb96b2b01b66236b7904456563edb
jumaevaiuz-oss <jumaev.aiuz@gmail.com>
2026-08-18 13:43:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json
 
 app = Flask(__name__)
 
-API_KEY = os.environ.get("API_KEY", "apikeyim")
+API_KEY = os.environ.get("API_KEY", "Kodim77a667_yz")
 DOWNLOAD_DIR = "/tmp/downloads"
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 MAX_SIZE_BYTES = 50 * 1024 * 1024  # 50MB
